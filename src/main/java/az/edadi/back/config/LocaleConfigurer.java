9d172a02a4757670ab7efbc0063ef65d5a856c1b
ulvu <ulvu.suleyman.00@gmail.com>
2023-07-30 13:48:33 +0400

    fixed localeConfiguration missings

--- src/main/java/az/edadi/back/config/LocaleConfigurer.java
+++ src/main/java/az/edadi/back/config/LocaleConfigurer.java
@@ -1,6 +1,7 @@
 package az.edadi.back.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-
+@Configuration
 public class LocaleConfigurer extends AcceptHeaderLocaleResolver
         implements WebMvcConfigurer {
     List<Locale> LOCALES = Arrays.asList(
