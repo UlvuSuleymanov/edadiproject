9755f594021f76f10e633695e10e9512f038312a
ulvu <ulvu.suleyman.00@gmail.com>
2023-07-31 17:28:25 +0400

    remove unused dto - uniSummaryResponse

--- src/main/java/az/edadi/back/controller/UniversityController.java
+++ src/main/java/az/edadi/back/controller/UniversityController.java
@@ -1,55 +1,50 @@
 package az.edadi.back.controller;
 
- import az.edadi.back.service.SpecialityService;
- import az.edadi.back.service.UniversityService;
- import az.edadi.back.entity.university.University;
- import az.edadi.back.model.request.UniRequestModel;
- import az.edadi.back.model.response.UniResponseModel;
- import az.edadi.back.model.response.UniSummaryModel;
- import az.edadi.back.repository.UniversityRepository;
- import lombok.RequiredArgsConstructor;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.cache.annotation.Cacheable;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
- import org.springframework.security.access.prepost.PreAuthorize;
- import org.springframework.web.bind.annotation.*;
- import java.util.List;
+import az.edadi.back.service.UniversityService;
+import az.edadi.back.entity.university.University;
+import az.edadi.back.model.request.UniRequestModel;
+import az.edadi.back.model.response.UniResponseModel;
+import az.edadi.back.repository.UniversityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/university")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 
 public class UniversityController {
-    private  final UniversityRepository universityRepository;
-    private  final UniversityService universityService;
+    private final UniversityRepository universityRepository;
+    private final UniversityService universityService;
 
     @PostMapping
     @PreAuthorize("hasAuthority('admin:update')")
-    ResponseEntity addUniversity(@RequestBody UniRequestModel uniRequestModel){
+    ResponseEntity addUniversity(@RequestBody UniRequestModel uniRequestModel) {
         University university = universityService.createUni(uniRequestModel);
         universityRepository.save(university);
-        return  new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity(HttpStatus.OK);
 
     }
 
     @GetMapping
     @Cacheable("universities")
-    ResponseEntity getUniList(){
-     List<UniResponseModel> uniResponseModelList = universityService.getUnisList();
-     return ResponseEntity.ok(uniResponseModelList);
+    ResponseEntity getUniList() {
+        List<UniResponseModel> uniResponseModelList = universityService.getUnisList();
+        return ResponseEntity.ok(uniResponseModelList);
 
     }
 
     @GetMapping("/{abbrName}")
-    ResponseEntity getUni(@PathVariable String abbrName){
-     UniResponseModel uniResponseModel = universityService.getUni(abbrName);
-    return ResponseEntity.ok(uniResponseModel);
+    ResponseEntity getUni(@PathVariable String abbrName) {
+        UniResponseModel uniResponseModel = universityService.getUni(abbrName);
+        return ResponseEntity.ok(uniResponseModel);
     }
-
-
-
-
 
 
 }
