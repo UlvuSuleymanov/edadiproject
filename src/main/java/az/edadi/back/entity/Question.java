58ecb5e15d68a271dc80a6cf54d1d95e2feecdf9
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2023-08-23 15:08:33 +0400

    improve getting user ip feature

--- src/main/java/az/edadi/back/entity/Question.java
+++ src/main/java/az/edadi/back/entity/Question.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
--- src/main/java/az/edadi/back/utility/AuthUtil.java
+++ src/main/java/az/edadi/back/utility/AuthUtil.java
@@ -12,6 +12,39 @@ import java.util.List;
 import java.util.Optional;
 
 public class AuthUtil {
+
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    public static String getcurrentIp() {
+
+        if (RequestContextHolder.getRequestAttributes() == null) {
+            return "0.0.0.0";
+        }
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        for (String header: IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return ip;
+            }
+        }
+
+        return request.getRemoteAddr();
+    }
     public static Long getCurrentUserId() {
         if (userIsAuthenticated())
             return DataParser.objectToLong(SecurityContextHolder.getContext().getAuthentication().getCredentials());
@@ -25,8 +58,8 @@ public class AuthUtil {
     public static boolean userIsAuthenticated() {
         return !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken);
     }
-
-    public static String getcurrentIp(){
+    @Deprecated
+    public static String getcurrentIpAddress(){
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         Optional<String> ipHeader = Optional.ofNullable(request.getHeader("X-FORWARDED-FOR"));
