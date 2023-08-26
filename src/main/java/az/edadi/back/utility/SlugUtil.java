bbb47f143f05e9d5dac8ddc1579a3de1c99ec79a
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2023-08-26 14:34:20 +0400

    fix bug  non latin symbols in slug

--- src/main/java/az/edadi/back/utility/SlugUtil.java
+++ src/main/java/az/edadi/back/utility/SlugUtil.java
@@ -1,16 +1,20 @@
 package az.edadi.back.utility;
 
+import java.util.regex.Pattern;
+
 public class SlugUtil {
+    private static final Pattern NONLATIN = Pattern.compile("[^\\w-[ÜüÖöŞşÇçIıƏəĞğ ]]");
 
     public static String createSlug(String title, Long id) {
-        title=title.replaceAll("[?&%=!.,')(]","");
-        title = title.trim().replaceAll(" +", "-");
-        return title+"-"+id;
+        String slug = NONLATIN.matcher(title).replaceAll("");
+        slug = slug.trim().replaceAll(" +", "-");
+        slug = slug + "-" + id;
+        return slug;
     }
 
-    public static Long getId(String slug){
-        String crumbs[]=slug.split("-");
-        String id = crumbs[crumbs.length-1];
+    public static Long getId(String slug) {
+        String crumbs[] = slug.split("-");
+        String id = crumbs[crumbs.length - 1];
         return Long.valueOf(id);
     }
 }
