30d99e30beb42b5fa5294ac4472d4140aa269d82
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2024-03-15 18:54:35 +0400

    telete commented row

--- src/main/java/az/edadi/back/ApplicationStarter.java
+++ src/main/java/az/edadi/back/ApplicationStarter.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@PropertySource("classpath:application-${ENV}common.proporties")
-public class ApplicationStarter {
+ public class ApplicationStarter {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationStarter.class, args);
