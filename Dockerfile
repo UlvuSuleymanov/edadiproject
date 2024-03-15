4dc87cfd31878173ad49fc4814fa49c6a1895ade
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2024-03-15 18:55:58 +0400

    test2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:17-jdk-alpine
 WORKDIR /app
-COPY target/edauni.jar edauni.jar
+COPY target/edauni-0.0.1.jar edauni-0.0.1.jar
 EXPOSE 5000
-CMD ["java","-jar","edauni.jar"]
+CMD ["java","-jar","edauni-0.0.1.jar"]
--- src/main/java/az/edadi/back/ApplicationStarter.java
+++ src/main/java/az/edadi/back/ApplicationStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
- public class ApplicationStarter {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStarter.class, args);
-	}
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
 
 }
